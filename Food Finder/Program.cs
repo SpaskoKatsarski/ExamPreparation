e4fd1200f24ed417b9649b3f656f2de36349b68a
Spasko Katsarski <patsokatsarski1@gmail.com>
2022-06-23 20:40:06 +0300

    New tasks solved.

---
+++ Food Finder/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Food_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<char>> words = new Dictionary<string, List<char>>
+            {
+                { "pear", new List<char>() },
+                { "flour", new List<char>() },
+                { "pork", new List<char>() },
+                { "olive", new List<char>() }
+            };
+            // We don't want the letters to be repeated and when the array length is full, we have found the corresponding words.
+            
+            Queue<char> vowels = new Queue<char>(Console.ReadLine().Split().Select(char.Parse).ToArray());
+            Stack<char> consonants = new Stack<char>(Console.ReadLine().Split().Select(char.Parse).ToArray());
+            List<string> foundWords = new List<string>();
+
+            while (consonants.Count > 0)
+            {
+                char vowel = vowels.Dequeue();
+                char consonant = consonants.Pop();
+
+                foreach (var word in words)
+                {
+                    //key: word
+                    //value: List with already found letters
+
+                    if (word.Key.Contains(vowel) && !word.Value.Contains(vowel))
+                    {
+                        word.Value.Add(vowel);
+
+                        //if (word.Value.Count == word.Key.Length)
+                        //{
+                        //    foundWords.Add(word.Key);
+                        //    words.Remove(word.Key);
+                        //}
+                    }
+
+                    if (word.Key.Contains(consonant) && !word.Value.Contains(consonant))
+                    {
+                        word.Value.Add(consonant);
+
+                        //if (word.Value.Count == word.Key.Length)
+                        //{
+                        //    foundWords.Add(word.Key);
+                        //    words.Remove(word.Key);
+                        //}
+                    }
+                }
+
+                //Returning it back to the collection:
+                vowels.Enqueue(vowel);
+            }
+
+            Console.WriteLine($"Words found: {words.Count(w => w.Value.Count == w.Key.Length)}");
+            //Console.WriteLine($"Words found: {foundWords.Count}");
+            foreach (var word in words.Where(w => w.Value.Count == w.Key.Length))
+            {
+                Console.WriteLine(word.Key);
+            }
+
+            //foundWords.OrderBy(w => w.StartsWith("pe")).ThenBy(w => w.StartsWith("f")).ThenBy(w => w.StartsWith("po")).ThenBy(w => w.StartsWith("o"));
+
+            
+            //foreach (var word in foundWords)
+            //{
+            //    Console.WriteLine(word);
+            //}
+        }
+    }
+}
---
+++ PawnWars/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PawnWars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////index 0 -> index 0 = 0 (row), index 1 = 0 (col)
+            ////index 1 -> index 1 = 0 (row), index 1 = 0 (col)
+
+            //Dictionary<int[], string> positions = new Dictionary<int[], string> 
+            //{
+            //    { new int[2] { 0, 0 }, "a8"}
+            //};
+
+            char[,] chessBoard = new char[8, 8];
+
+            int whiteRow = 0;
+            int whiteCol = 0;
+
+            int blackRow = 0;
+            int blackCol = 0;
+
+            for (int row = 0; row < 8; row++)
+            {
+                char[] rowInfo = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < 8; col++)
+                {
+                    if (rowInfo[col] == 'w')
+                    {
+                        whiteRow = row;
+                        whiteCol = col;
+                    }
+                    else if (rowInfo[col] == 'b')
+                    {
+                        blackRow = row;
+                        blackCol = col;
+                    }
+
+                    chessBoard[row, col] = rowInfo[col];
+                }
+            }
+
+            while (true)
+            {
+                if (IsIndexValid(chessBoard, whiteRow - 1, whiteCol - 1) && chessBoard[whiteRow - 1, whiteCol - 1] == 'b')
+                {
+                    //We capture the black at the top left corner
+                    char winLetter = GiveWinLetter(blackCol);
+                    char winNumber = GiveWinNumber(blackRow);
+
+                    Console.WriteLine($"Game over! White capture on {winLetter}{winNumber}."); //coordinates of black pawn
+                    return;
+                }
+                else if (IsIndexValid(chessBoard, whiteRow - 1, whiteCol + 1) && chessBoard[whiteRow - 1, whiteCol + 1] == 'b')
+                {
+                    //We capture the black at the top right corner
+                    char winLetter = GiveWinLetter(blackCol);
+                    char winNumber = GiveWinNumber(blackRow);
+
+                    Console.WriteLine($"Game over! White capture on {winLetter}{winNumber}."); //coordinates of black pawn
+                    return;
+                }
+                else
+                {
+                    whiteRow--;
+                }
+
+                if (whiteRow == 0)
+                {
+                    char finishLetter = FindFinishLetter(whiteCol);
+
+                    Console.WriteLine($"Game over! White pawn is promoted to a queen at {finishLetter}8.");
+                    return;
+                }
+                else
+                {
+                    chessBoard[whiteRow + 1, whiteCol] = '-';
+                    chessBoard[whiteRow, whiteCol] = 'w';
+                }
+
+
+
+                //---------------Black Logic--------------------
+                if (IsIndexValid(chessBoard, blackRow + 1, blackRow - 1) && chessBoard[blackRow + 1, blackCol - 1] == 'w')
+                {
+                    //We capture the white at the bottom left corner
+                    char winLetter = GiveWinLetter(whiteCol);
+                    char winNumber = GiveWinNumber(whiteRow);
+
+                    Console.WriteLine($"Game over! Black capture on {winLetter}{winNumber}."); //coordinates of white pawn
+                    return;
+                }
+                else if (IsIndexValid(chessBoard, blackRow + 1, blackRow + 1) && chessBoard[blackRow + 1, blackRow + 1] == 'w')
+                {
+                    //We capture the white at the bottom right corner
+                    char winLetter = GiveWinLetter(whiteCol);
+                    char winNumber = GiveWinNumber(whiteRow);
+
+                    Console.WriteLine($"Game over! Black capture on {winLetter}{winNumber}."); //coordinates of white pawn
+                    return;
+                }
+                else
+                {
+                    blackRow++;
+                }
+
+                if (blackRow == 7)
+                {
+                    char finishLetter = FindFinishLetter(blackCol);
+
+                    Console.WriteLine($"Game over! Black pawn is promoted to a queen at {finishLetter}1.");
+                    return;
+                }
+                else
+                {
+                    chessBoard[blackRow - 1, blackCol] = '-';
+                    chessBoard[blackRow, blackCol] = 'b';
+                }
+            }
+        }
+
+        public static bool IsIndexValid(char[,] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.GetLength(0) && col >= 0 && col < matrix.GetLength(1);
+        }
+
+        public static char FindFinishLetter(int col) 
+        {
+            if (col == 0)
+            {
+                return 'a';
+            }
+            else if (col == 1)
+            {
+                return 'b';
+            }
+            else if (col == 2)
+            {
+                return 'c';
+            }
+            else if (col == 3)
+            {
+                return 'd';
+            }
+            else if (col == 4)
+            {
+                return 'e';
+            }
+            else if (col == 5)
+            {
+                return 'f';
+            }
+            else if (col == 6)
+            {
+                return 'g';
+            }
+            else if (col == 7)
+            {
+                return 'h';
+            }
+
+            return 'x';
+        }
+
+        public static char GiveWinNumber(int row)
+        {
+            if (row == 0)
+            {
+                return '8';
+            }
+            else if (row == 1)
+            {
+                return '7';
+            }
+            else if (row == 2)
+            {
+                return '6';
+            }
+            else if (row == 3)
+            {
+                return '5';
+            }
+            else if (row == 4)
+            {
+                return '4';
+            }
+            else if (row == 5)
+            {
+                return '3';
+            }
+            else if (row == 6)
+            {
+                return '2';
+            }
+            else if (row == 7)
+            {
+                return '1';
+            }
+
+            return 'x';
+        }
+
+        public static char GiveWinLetter(int col)
+        {
+            if (col == 0)
+            {
+                return 'a';
+            }
+            else if (col == 1)
+            {
+                return 'b';
+            }
+            else if (col == 2)
+            {
+                return 'c';
+            }
+            else if (col == 3)
+            {
+                return 'd';
+            }
+            else if (col == 4)
+            {
+                return 'e';
+            }
+            else if (col == 5)
+            {
+                return 'f';
+            }
+            else if (col == 6)
+            {
+                return 'g';
+            }
+            else if (col == 7)
+            {
+                return 'h';
+            }
+
+            return 'x';
+        }
+    }
+}
